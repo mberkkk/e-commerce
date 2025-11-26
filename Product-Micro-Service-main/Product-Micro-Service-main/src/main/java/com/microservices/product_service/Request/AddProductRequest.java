26a44a4c07e3de1fee0e58d3db47ccdaef8ede8f
Emre Berk Göğer <emrbrkg@gmail.com>
2025-11-26 17:53:20 +0300

    maven files added

--- Product-Micro-Service-main/Product-Micro-Service-main/src/main/java/com/microservices/product_service/Request/AddProductRequest.java
+++ Product-Micro-Service-main/Product-Micro-Service-main/src/main/java/com/microservices/product_service/Request/AddProductRequest.java
@@ -1,11 +1,6 @@
 package com.microservices.product_service.Request;
 
-import com.microservices.product_service.DTO.ProductDTO;
-import com.microservices.product_service.Entity.Category;
-import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 
@@ -14,6 +9,7 @@ public class AddProductRequest {
     private String name;
     private String description;
     private BigDecimal price;
+    private String currency;
     private Integer stockQuantity;
     private String imageUrl;
     private Integer popularityScore;
